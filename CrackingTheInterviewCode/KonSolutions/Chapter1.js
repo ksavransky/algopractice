ccffea1994f103cc8754271ea858e37fc718645b
Konstantin Savransky <ksavransky@gmail.com>
2018-11-29 09:27:58 -0800

    change

--- CrackingTheInterviewCode/KonSolutions/Chapter1.js
+++ CrackingTheInterviewCode/KonSolutions/Chapter1.js
@@ -143,18 +143,3 @@ const matrix = [
 ]
 
 // console.log(zeroMakesZero(matrix))
-
-
-// Assume you have a method isSubstring which checks if one word is a substring of another
-// Given two strings, s1 and s2 (the substring), write code to check if
-// s2 is a rotation of s1 using only one call to isSubstring (i e , “waterbottle” is a rotation of “erbottlewat”)
-
-function isSubstring(s1, s2){
-  return s1.indexOf(s2) !== -1
-}
-
-console.log(isSubstring('ok thank you', 'thank'))
-
-function isRotation(s1, s2) {
-
-}
