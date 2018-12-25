670c9d2e25663948ce7af0d4319b625c927054af
Konstantin Savransky <ksavransky@gmail.com>
2018-12-25 13:49:33 -0800

    another

--- CrackingTheInterviewCode/KonSolutions/Chp2LinkedListsPart2.js
+++ CrackingTheInterviewCode/KonSolutions/Chp2LinkedListsPart2.js
@@ -103,7 +103,7 @@ listNum2.insertNodeLast(sN5)
 listNum2.insertNodeLast(sN9)
 listNum2.insertNodeLast(sN2)
 
-function sumLists(list1, list2) {
+function sumListsKonSolution(list1, list2) {
   const num1Array = []
   let currentNode = list1.head
   while (currentNode) {
@@ -118,8 +118,115 @@ function sumLists(list1, list2) {
     currentNode = currentNode.next
   }
 
-  console.log('num1Array', num1Array)
-  console.log('num2Array', num2Array)
+  const answer = parseInt(num1Array.reverse().join(''), 10) + parseInt(num2Array.reverse().join(''))
+  const answerArray= answer.toString().split('').reverse()
+  const answerList = new DoubleLinkedList()
+  for (let i = 0; i < answerArray.length; i++) {
+    answerList.insertNodeLast(new Node(parseInt(answerArray[i], 10)))
+  }
+  return answerList
+
 }
 
-sumLists(listNum1, listNum2)
+// console.log(sumListsKonSolution(listNum1, listNum2))
+
+function sumListsBetterSolution(list1, list2) {
+  let carry = 0
+  let list1Node = list1.head
+  let list2Node = list2.head
+  const answerList = new DoubleLinkedList()
+  while(list1Node && list2Node) {
+    const currentValue = list1Node.value + list2Node.value + carry
+    const oneDigit = currentValue % 10
+    carry = (currentValue - oneDigit) / 10
+    answerList.insertNodeLast(new Node(oneDigit))
+    list1Node = list1Node.next
+    list2Node = list2Node.next
+  }
+  return answerList
+}
+
+// console.log(sumListsBetterSolution(listNum1, listNum2))
+
+
+// pretend these are single linked list... obviously easier with double
+const palindromeListEven = new DoubleLinkedList()
+const palindromeListOdd = new DoubleLinkedList()
+const notPalindromeListEven = new DoubleLinkedList()
+const notPalindromeListOdd = new DoubleLinkedList()
+
+const pN1 = new Node(1)
+const pN2 = new Node(2)
+const pN3 = new Node(3)
+const pN1b = new Node(1)
+const pN2b = new Node(2)
+const pN3b = new Node(3)
+const pN4 = new Node(4)
+
+// palindromeListEven.insertNodeLast(pN1)
+// palindromeListEven.insertNodeLast(pN2)
+// palindromeListEven.insertNodeLast(pN3)
+// palindromeListEven.insertNodeLast(pN3b)
+// palindromeListEven.insertNodeLast(pN2b)
+// palindromeListEven.insertNodeLast(pN1b)
+
+// palindromeListOdd.insertNodeLast(pN1)
+// palindromeListOdd.insertNodeLast(pN2)
+// palindromeListOdd.insertNodeLast(pN3)
+// palindromeListOdd.insertNodeLast(pN4)
+// palindromeListOdd.insertNodeLast(pN3b)
+// palindromeListOdd.insertNodeLast(pN2b)
+// palindromeListOdd.insertNodeLast(pN1b)
+//
+// notPalindromeListEven.insertNodeLast(pN1)
+// notPalindromeListEven.insertNodeLast(pN2)
+// notPalindromeListEven.insertNodeLast(pN3)
+// notPalindromeListEven.insertNodeLast(pN4)
+// notPalindromeListEven.insertNodeLast(pN2b)
+// notPalindromeListEven.insertNodeLast(pN1b)
+//
+// notPalindromeListOdd.insertNodeLast(pN1)
+// notPalindromeListOdd.insertNodeLast(pN2)
+// notPalindromeListOdd.insertNodeLast(pN4)
+// notPalindromeListOdd.insertNodeLast(pN3b)
+// notPalindromeListOdd.insertNodeLast(pN1b)
+
+function isPalindrome(list) {
+  if (!list.head) {
+    return false
+  }
+  if (!list.head.next) {
+    return true
+  }
+  if (!list.head.next.next) {
+    return list.head.value === list.head.next.value
+  }
+
+  let currentNode = list.head
+  let runnerNode = list.head.next
+  let isEvenLengthList = true
+  const halfwayArray = []
+  while(runnerNode) {
+    halfwayArray.push(currentNode.value)
+    currentNode = currentNode.next
+    if (runnerNode.next && !runnerNode.next.next) {
+      isEvenLengthList = false
+    }
+    runnerNode = runnerNode.next ? runnerNode.next.next : false
+  }
+
+  const halfwayArrayReversed = halfwayArray.reverse()
+  let index = 0
+
+  currentNode = isEvenLengthList ? currentNode : currentNode.next
+  while(currentNode) {
+    if (currentNode.value !== halfwayArrayReversed[index]) {
+      return false
+    }
+    index++
+    currentNode = currentNode.next
+  }
+  return true
+}
+
+// console.log(isPalindrome(notPalindromeListOdd))
