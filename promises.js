05a430010f4e86e07bf577a2115e67f7e5a3cd0b
Konstantin Savransky <ksavransky@gmail.com>
2019-01-19 17:22:34 -0800

    promises

--- promises.js
+++ promises.js
@@ -21,6 +21,13 @@ momsPromise.then((value) => {
   console.log("Mom coudn't buy me the phone because ", reason);
 });
 
+// .finally doesn't work in node
+// momsPromise.finally(() => {
+//   console.log(
+//     "Irrespecitve of whether my mom can buy me a phone or not, I still love her"
+//   );
+// });
+
 
 // E.g.
 // function myAsyncFunction(url) {
@@ -33,19 +40,75 @@ momsPromise.then((value) => {
 //   });
 // }
 
-// let myFirstPromise = new Promise((resolve, reject) => {
-//   // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
-//   // In this example, we use setTimeout(...) to simulate async code.
-//   // In reality, you will probably be using something like XHR or an HTML5 API.
-//   setTimeout(function(){
-//     resolve("Success!"); // Yay! Everything went well!
-//   }, 2000);
+let myFirstPromise = new Promise((resolve, reject) => {
+  // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
+  // In this example, we use setTimeout(...) to simulate async code.
+  // In reality, you will probably be using something like XHR or an HTML5 API.
+  setTimeout(function(){
+    resolve("Success!"); // Yay! Everything went well!
+  }, 2000);
+});
+
+myFirstPromise.then((successMessage) => {
+  // successMessage is whatever we passed in the resolve(...) function above.
+  // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
+  console.log("Finally: " + successMessage);
+});
+
+console.log(myFirstPromise)
+
+
+// -------------------------------------
+// https://hackernoon.com/understanding-promises-in-javascript-13d99df067c1
+
+// Every promise has a STATUS and VALUE
+
+// STATE: pending, resolved, or rejected
+// VALUE: resolve or reject value or undefined (while pending)
+
+// let keepsHisWord = true;
+// let promise1 = new Promise(function(resolve, reject) {
+//   if (keepsHisWord) {
+//     resolve("The man likes to keep his word");
+//   } else {
+//     reject("The man doesnt want to keep his word");
+//   }
 // });
-//
-// myFirstPromise.then((successMessage) => {
-//   // successMessage is whatever we passed in the resolve(...) function above.
-//   // It doesn't have to be a string, but if it is only a succeed message, it probably will be.
-//   console.log("Yay! " + successMessage);
+// console.log(promise1);
+// Promise {
+//   <resolved>: "The man likes to keep his word"}
+//   __proto__: Promise[[PromiseStatus]]: "resolved"
+//             [[PromiseValue]]: "The man likes to keep his word"
+// }
+
+// let promise2 = new Promise(function(resolve, reject) {
+//   setTimeout(function() {
+//     resolve(console.log('finally resolved jeez'));
+//   }, 5 * 1000);
 // });
+// NOTE NOTE NOTE: We don't call the promise2 with promise2() -- but it still runs!!!!!!
+// console.log(promise2); // Promise { <pending> }
+// 5 seconds later console logs 'finally resolved jeez'
+
+
+// let keepsHisWord2 = false;
+// let promise3 = new Promise(function(resolve, reject) {
+//   if (keepsHisWord2) {
+//     resolve("The man likes to keep his word");
+//   } else {
+//     reject("The man doesn't want to keep his word");
+//   }
+// });
+// console.log(promise3);
+// Promise { <rejected> 'The man doesn\'t want to keep his word' }
+
+
 //
-// console.log(myFirstPromise())
+// Promise.prototype.catch(onRejected)
+//
+// Promise.prototype.then(onFulfilled, onRejected)
+//
+// Promise.prototype.finally(onFinally)
+
+// .finally is declared for a promise then it will be executed whenever a promise is settled
+// irrespective of whether it is fulfilled or rejected
