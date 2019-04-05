8763acd64fdf6cb81c01e41bfd36d224dc4caecd
Konstantin Savransky <ksavransky@gmail.com>
2019-04-05 09:21:45 -0700

    more

--- CrackingTheInterviewCode/moderate.js
+++ CrackingTheInterviewCode/moderate.js
@@ -3,19 +3,35 @@
 // (represented as a start point and an end point), compute the point of intersection, if any.
 
 
-function cellsInLine(pointOne, pointTwo) {
-  cellsInLine = []
-
-  return cellsInLine
+function getCellsInLine(pointOne, pointTwo, cellsInLine = []) {
+  cellsInLine.push(pointOne)
+  if (pointOne[0] === pointTwo[0] && pointOne[1] === pointTwo[1]) {
+    return cellsInLine
+  }
+  let differenceBetweenXs = pointOne[0] - pointTwo[0]
+  let differenceBetweenYs = pointOne[1] - pointTwo[1]
+  if (differenceBetweenXs !== 0) {
+    differenceBetweenXs = differenceBetweenXs / Math.abs(differenceBetweenXs)
+  }
+  if (differenceBetweenYs !== 0) {
+    differenceBetweenYs = differenceBetweenYs / Math.abs(differenceBetweenYs)
+  }
+  return getCellsInLine([pointOne[0] - differenceBetweenXs, pointOne[1] - differenceBetweenYs], pointTwo, cellsInLine)
 }
 
-function intersection(lineOne, lineTwo) {
+console.log(getCellsInLine([0, 0], [3, 3]))
+
+// function intersection(lineOne, lineTwo) {
+//
+//
+// }
+
+// my thoughts
+// one line2 point must be below one of the line1 points and the other line2 point must be above one of the line1 points
+// ???
 
 
-}
-
-
-const line1 = [[0, 0], [3, 3]]
-const line2 = [[0, 1], [0, 4]]
-console.log(intersection(line1, line2))
+// const line1 = [[0, 0], [3, 3]]
+// const line2 = [[0, 1], [0, 4]]
+// console.log(intersection(line1, line2))
 // output: [1, 1]
